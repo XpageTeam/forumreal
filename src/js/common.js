ba8b7ea42645a8121a783d98197047fac2c9643d
SergeyG <sergey1008@gmail.com>
2019-06-27 18:08:00 +0500

    начал адаптив

--- src/js/common.js
+++ src/js/common.js
@@ -13,6 +13,7 @@ if (!is.touchDevice())
 
 $(function(){
 
+	
 
 	$(".slider-3 .slider__list").slick({
 		slidesToShow: 3,
@@ -60,54 +61,54 @@ $(function(){
 
 
 
-(function() {
+// (function() {
 
-  // проверяем поддержку
-  if (!Element.prototype.closest) {
+//   // проверяем поддержку
+//   if (!Element.prototype.closest) {
 
-    // реализуем
-    Element.prototype.closest = function(css) {
-      var node = this;
+//     // реализуем
+//     Element.prototype.closest = function(css) {
+//       var node = this;
 
-      while (node) {
-        if (node.matches(css)) return node;
-        else node = node.parentElement;
-      }
-      return null;
-    };
-  }
+//       while (node) {
+//         if (node.matches(css)) return node;
+//         else node = node.parentElement;
+//       }
+//       return null;
+//     };
+//   }
 
-})();
+// })();
 
-try{
-	document.addEventListener("DOMContentLoaded", e => {
-		require("./jquery.fancybox.js")
-		require("../css/jquery.fancybox.css")
+// try{
+// 	document.addEventListener("DOMContentLoaded", e => {
+// 		require("./jquery.fancybox.js")
+// 		require("../css/jquery.fancybox.css")
 
-		$(".fancybox").fancybox({
-			trapFocus: false,
-			touch: false,
-			loop: true,
-			buttons: ["fullscreen", "slideShow", "close", "thumbs"],
-			image: {
-				preload: true,
-			},
-			transitionEffect: "slide",
-		})
+// 		$(".fancybox").fancybox({
+// 			trapFocus: false,
+// 			touch: false,
+// 			loop: true,
+// 			buttons: ["fullscreen", "slideShow", "close", "thumbs"],
+// 			image: {
+// 				preload: true,
+// 			},
+// 			transitionEffect: "slide",
+// 		})
 
 
-		// if (!is.touchDevice()){
-		// 	window.selectizeOpen = false;
-			// $(".selectize").selectize()
+// 		// if (!is.touchDevice()){
+// 		// 	window.selectizeOpen = false;
+// 			// $(".selectize").selectize()
 
-		// }
+// 		// }
 
-		alert(1)
+// 		alert(1)
 				
 		
 
 		
-	})
-}catch(e){
-	console.log(e)
-}
+// 	})
+// }catch(e){
+// 	console.log(e)
+// }
