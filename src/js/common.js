1bf17f8a440f61f787cdc586356d1e0707e5ce31
SergeyG <sergey1008@gmail.com>
2019-06-28 14:58:15 +0500

    закончил адаптив

--- src/js/common.js
+++ src/js/common.js
@@ -20,12 +20,6 @@ $(function(){
 		slidesToScroll: 1,
 		appendArrows: $('.slider-arrow'),
 		responsive: [
-			{
-	         breakpoint: 1200,
-	         settings: {
-	           slidesToShow: 2,
-	         }
-	       },
 	       {
 	         breakpoint: 600,
 	         settings: {
@@ -35,6 +29,22 @@ $(function(){
 	       
 	    ]
 	})
+
+	$(".slider-2 .slider__list").slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		appendArrows: $('.slider-2 .slider-arrow'),
+		responsive: [
+	       {
+	         breakpoint: 600,
+	         settings: {
+	           slidesToShow: 1,
+	         }
+	       },
+	       
+	    ]
+	})
+
 
 	$(".best-offer__list").slick({
 		slidesToShow: 3,
@@ -42,7 +52,7 @@ $(function(){
 		appendArrows: $('.best-offer .slider-arrow'),
 		responsive: [
 			{
-	         breakpoint: 1200,
+	         breakpoint: 1000,
 	         settings: {
 	           slidesToShow: 2,
 	         }
@@ -57,8 +67,40 @@ $(function(){
 	    ]
 	})
 
-})
 
+
+
+
+})
+// 	var calculator = new Vue({
+// 		el: '#calculator',
+// 		data: {
+// 			price: '',
+// 			downPayment: '',
+// 			tradeIn: '',
+// 			length: '60',
+// 			rate: '',
+// 			calcPayment: ''
+// 		},
+// 		computed: {
+// 			calcPayment: function(e){
+// 				var p = this.price - this.downPayment - this.tradeIn;
+// 				var r = this.rate / 1200;
+// 				var n = this.length;
+// 				var i = Math.pow((1+r),n);
+// 				var payment = ( p * r * i) / (i - 1) || 0;
+// 				return currencyFormat(payment);
+// 			},
+// 			numFormat: function(e){
+// 				e.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+// 			}
+// 		}
+		
+// 	});
+
+// function currencyFormat (num) {
+//     return "$" + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
+// }
 
 
 // (function() {
